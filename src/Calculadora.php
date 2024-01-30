bf2fb4daa38cadc4dd00fee134cfc2e0f62a11b8
Aikarubi <nerupe@floridauniversitaria.es>
2024-01-30 12:35:41 +0100

    AAAAAAAAAAAA

---
+++ src/Calculadora.php
@@ -0,0 +1,57 @@
+<?php
+class Calculadora
+{
+    private $num1;
+    private $num2;
+    
+    public function __construct($a1,$a2)
+    {
+        $this->num1=$a1;
+        $this->num2=$a2;
+    }
+    
+    public function suma(){
+        return $this->num1+$this->num2;
+    }
+
+    public function resta(){
+        if ($this->num1 > $this->num2)
+            return $this->num1-$this->num2;
+        else
+            return $this->num2-$this->num1;
+    }
+}
+
+/*
+
+class Calculadora
+{
+    private $num1;
+    private $num2;
+    
+    public function __construct($a1,$a2)
+    {
+        $this->num1=$a1;
+        $this->num2=$a2;
+    }
+    
+    public function suma(){
+        return $this->num1+$this->num2;
+    }
+
+    public function diff(){
+        return $this->num1-$this->num2;
+
+    }
+
+    public function mult(){
+        return $this->num1*$this->num2;
+    }
+
+    public function div(){
+        return $this->num1/$this->num2;
+        
+    }
+}
+*/
+?>
